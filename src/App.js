ea755bcdcd0bd95b5b3db3b20e861d781f5855ea
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2022-01-18 23:45:35 -0500

    Added some Home content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import './index.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/home/home.component';
@@ -12,7 +13,7 @@ import appConfig from './contexts/app-config/config.json';
 import AboutUs from './components/about/about-us.component';
 import Giving from './components/giving/giving.component';
 import Connect from './components/connect/connect.component';
-import { Container, ThemeProvider } from '@mui/material';
+import { Box, ThemeProvider } from '@mui/material';
 import { theme } from './Theme';
 import Media from './components/media/media.component';
 import { Footer } from './components/footer/footer.component';
@@ -25,7 +26,7 @@ function App() {
           <CssBaseline />
           <ThemeProvider theme={theme}>
             <ResponsiveAppBar />
-            <Container className="content">
+            <Box className="content">
               <Routes>
                 <Route path="/" exact element={<Home />} />
                 <Route path="/media" exact element={<Media />} />
@@ -36,7 +37,7 @@ function App() {
                 <Route path="/giving" exact element={<Giving />} />
                 <Route path="/connect" exact element={<Connect />} />
               </Routes>
-            </Container>
+            </Box>
             <Footer />
           </ThemeProvider>
         </React.Fragment>
--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -1,14 +1,41 @@
-import { Typography } from '@mui/material';
-import React, { Component } from 'react';
+import { Box, Container } from '@mui/material';
+import React, { useContext } from 'react';
+import { AppConfigContext } from '../../contexts/app-config/app-config.service';
+import { HomeMainContent } from './home-intro-content.component';
+import './home.component.css';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <main>
-        <Typography variant="h4" component="h1">
-          Welcome
-        </Typography>
-      </main>
-    );
-  }
-}
+const homeStyle = (path) => {
+  return {
+    height: '100vh',
+    width: '100%',
+    backgroundImage: `url(${path})`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundAttachment: 'fixed',
+  };
+};
+
+export const Home = () => {
+  const homeConfigContext = useContext(AppConfigContext).website.home;
+  const bgImage = homeConfigContext.intro_section.images.background_image;
+  const bgImagePath = '/images/home/' + bgImage.name;
+
+  return (
+    <Box component="main" style={homeStyle(bgImagePath)}>
+      <Container
+        component="article"
+        sx={{
+          display: 'grid',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}
+      >
+        <HomeMainContent />
+      </Container>
+    </Box>
+  );
+};
+
+export default Home;
